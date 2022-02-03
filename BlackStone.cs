488985c7eaa760dafe119e72e289ad3fdc52fa7a
JuyoungKim90 <68528090+JuyoungKim90@users.noreply.github.com>
2022-02-03 12:30:01 +0900

    Add files via upload

---
+++ BlackStone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Gomoku
+{
+    public class BlackStone : Stone
+    {
+        public StoneColor Color
+        {
+            get { return color; }
+        }
+        public char Graph
+        {
+            get { return shape; }
+        }
+
+        public BlackStone()
+        {
+            color = StoneColor.Black;
+            shape = '●';
+        }
+    }
+}
---
+++ HumanPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gomoku
+{
+    class HumanPlayer : Player
+    {
+        
+        public HumanPlayer(string ID, bool ishuman) // containner
+        {
+            playerID = ID;
+            gamemodeHuman = ishuman;
+        }
+        
+    }
+}
+
