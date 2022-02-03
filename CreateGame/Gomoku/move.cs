608b231193afd2db2eccaad540d84d686b93e896
JuyoungKim90 <68528090+JuyoungKim90@users.noreply.github.com>
2022-02-03 12:44:19 +0900

    Object oriented design

---
+++ CreateGame/Gomoku/move.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Gomoku
+{
+    public abstract class Move :Board
+    {
+        
+        public virtual void SetStone(int x, int y, Stone targetStone)
+        {
+            Stonelocation[x, y] = targetStone;
+            lastX = x;
+            lastY = y;
+        }
+
+        public virtual bool IsValidPosition(int x, int y)
+        {
+            if (x > this.rowSize - 1)
+                return false;
+            if (y > this.columnSize - 1)
+                return false;
+            Stone stone = Stonelocation[x, y];
+            if (stone == null)
+                return true;
+            else
+                return false;
+        }
+        public virtual void Undo()
+        {
+            if (lastX > this.rowSize - 1 || lastX < 0)
+                return;
+            if (lastY > this.columnSize - 1 || lastY < 0)
+                return;
+            Stonelocation[lastX, lastY] = null;
+            lastX = -1;
+            lastY = -1;
+        }
+
+        public virtual void ShowBoard()
+        {
+
+        }
+
+
+    }
+}
